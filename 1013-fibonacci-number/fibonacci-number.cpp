1700546b6d0859148ff2d86c7cbc2b228457be4f
Sumit Singh <singhakash16810@gmail.com>
2026-08-21 15:20:34 +0530

    Time: 10 ms (45.68%) | Memory: 7.8 MB (65.72%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-
-         // Base case
-
+        // base case
+        
         if(n==0){
             return 0;
         }
-
         if(n==1){
             return 1;
         }
 
         // recursive case
-
         int ans = fib(n-1)+fib(n-2);
         return ans;
         
