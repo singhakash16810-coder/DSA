60cadf30f53fbc1918659be46444225c95b176a0
Sumit Singh <singhakash16810@gmail.com>
2026-08-29 23:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (36.81%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -10,7 +10,7 @@ public:
            
       for(int i=0;i<n;i++){
         int s =0;
-        int e  =n -1;
+        int e =n -1;
         while(s<e){
             swap(matrix[i][s],matrix[i][e]);
             s++;
